d28fc80e8ab5a3dca015d9258dd20b9738238231
Yashwanth-dev-ops <yashwanthbathina76@gmail.com>
2025-09-15 21:39:29 +0530

    Delete krishna-university-face-attendance version3 directory

--- krishna-university-face-attendance version3/components/Spinner.tsx
+++
@@ -1,8 +0,0 @@
-
-import React from 'react';
-
-export const Spinner: React.FC = () => {
-    return (
-        <div className="w-16 h-16 border-4 border-t-4 border-gray-200 border-t-blue-500 rounded-full animate-spin"></div>
-    );
-};
